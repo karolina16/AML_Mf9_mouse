b7e7190f021e62e4335c1b85ddad47481eabaeba
kzk <karolina.zielinska@csem.ch>
2023-11-09 14:28:52 +0100

    MLL-AF9 and its targets analysis in data from Basel

---
+++ Scripts_AML_mouse/Basel_Data_EDA.R
@@ -0,0 +1,40 @@
+library(SingleCellExperiment)
+library(strex)
+library(purrr)
+library(visNetwork)
+library(dplyr)
+library(scater)
+library(scran)
+library(batchelor)
+library(bluster)
+library(tidyr)
+library(here)
+library(dittoSeq)
+
+#### load the data
+data_bs <- readRDS(here("Data_AML_mouse/Data_Basel/transfer_184642_files_3dc8eea7/sce_withTSNE_29092023.rds"))
+data_bs
+
+#### explore cell annotation
+table(data_bs$final_annotation)
+table(data_bs$Type)
+table(data_bs$Batch)
+head(colData(data_bs))
+
+plotReducedDim(data_bs, dimred = "UMAP_MNN_Sample_k50_cellcycle", colour_by = "final_annotation")
+plotReducedDim(data_bs, dimred = "UMAP_MNN_Sample_k50_cellcycle", colour_by = "Batch")
+
+#### check the MLL-AF9 expression and its target genes
+fusion_gene_targets <- c("Meis1", "Hoxa9", "Eya1")
+
+plotReducedDim(data_bs, dimred = "UMAP_MNN_Sample_k50_cellcycle", colour_by = "MLL-AF9")
+
+multi_dittoPlot(data_bs, vars = "MLL-AF9", 
+                group.by = "final_annotation", plots = c("jitter", "vlnplot", "boxplot"), 
+                ylab = "Lognormalized counts", ncol = 1,
+                theme = theme_classic() + theme(plot.title = element_text(size = 10)))
+
+multi_dittoPlot(data_bs, vars = fusion_gene_targets, 
+                group.by = "final_annotation", plots = c("jitter", "vlnplot", "boxplot"), 
+                ylab = "Lognormalized counts", ncol = 3,
+                theme = theme_classic() + theme(plot.title = element_text(size = 10)))
