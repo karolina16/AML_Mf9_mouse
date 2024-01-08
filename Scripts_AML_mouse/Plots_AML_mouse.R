41db67736402a138074b865cd08c90adaf12cba1
kzk <karolina.zielinska@csem.ch>
2024-01-08 15:15:18 +0100

    split code between ECs and MSC for time point analysis

--- Scripts_AML_mouse/Plots_AML_mouse.R
+++ Scripts_AML_mouse/Plots_AML_mouse.R
@@ -13,17 +13,19 @@ data_ecs<- readRDS(here("R_objects_AML_mouse/AML_Exp1_Exp2/AML_Exp1_Exp2_Harmoni
 
 data_fibros_filt <- data_fibros[,!duplicated(colnames(data_fibros))]
 data_ecs_filt <- data_ecs[,!duplicated(colnames(data_ecs))]
-
+# remove 3 cells from D60 in cluster 1 for plotting
+d60_ecs1 <- colnames(data_ecs_filt[,data_ecs_filt$label == "1" & data_ecs_filt$time_point == "D60"])
+data_ecs_filt2 <- data_ecs_filt[,!colnames(data_ecs_filt) %in% d60_ecs1]
 
 #### quick visual check
 plotReducedDim(data_fibros, dimred = "UMAP_harmony", colour_by="label", text_by="label")
 plotReducedDim(data_fibros, dimred = "UMAP_harmony", 
-               colour_by="Cdh2")
+               colour_by="Nos1")
 
 
 plotReducedDim(data_ecs, dimred = "UMAP_harmony", colour_by="label", text_by="label")
 plotReducedDim(data_ecs, dimred = "UMAP_harmony", 
-               colour_by="Myh9")
+               colour_by="Spp1")
 
 
 #### split UMAP by time point
@@ -140,6 +142,7 @@ multi_dittoPlot(data_ecs_filt, vars = fusion_gene_targets,
                 ylab = "Lognormalized counts", ncol = 3,
                 theme = theme_classic() + theme(plot.title = element_text(size = 10)))
 
+# group Mes by label and time point 
 for (i in 1:length(fusion_gene_targets)) {
   dittoPlot(data_fibros_filt, fusion_gene_targets[i], group.by = "time_point", 
             split.by = "label",
@@ -154,10 +157,10 @@ for (i in 1:length(fusion_gene_targets)) {
             split.adjust = list(scales = "free_y"), max = NA)
   ggsave(here("Plots_AML_mouse/Plots_Exp1_Exp2_Con_EDA/Plots_Ex1_Exp2_Fibros_EDA//", paste(fusion_gene_targets[i], "_Mes_FusionTargets_TimePoint_AML_Violin.pdf", sep = "")), width = 10,  height = 7)
   
-}
+} 
 
 
-# split by time_point
+# split ECs by time_point
 for (i in 1:length(fusion_gene_targets)) {
   dittoDimPlot(data_ecs_filt, var=fusion_gene_targets[i], reduction.use = "UMAP_harmony", 
                split.by = c("time_point"),
@@ -197,25 +200,6 @@ markers_CAF <- c("Fap", "S100a4", "Pdgfra", "Pdgfrb", "Vim", "Cav1", "Postn",
                  "Tagln", "Itga11", "Col11a1", "Mfap5", "Cd34", "Ly6a", "Cd44", "Thy1")
 
 multi_dittoPlot(data_fibros_filt, vars = markers_CAF, 
-                group.by = "label", plots = c("jitter", "vlnplot", "boxplot"), 
-                ylab = "Lognormalized counts", ncol = 3,
-                theme = theme_classic() + theme(plot.title = element_text(size = 10)))
-
-#### plot barrier integrity markers in ECs
-markers_bar_int <- c("Dock1", "Elmo1", "Nfe2l2", "Eng", "Kdr", "Cdh5", "Tjp1", 
-                     "Myl2")
-
-nrf2_targets <- c("Ulk1", "Atg5", "Cul3", "Gclc", "Gclm", "Keap1", "S1pr1",
-                     "Hmox1","Cat", "P4hb", "Sema6a", "Il6")
-
-dittoDotPlot(data_ecs_filt, vars = markers_bar_int, group.by  = "label",
-             scale = FALSE, size = 12) + theme(text = element_text(size = 25)) 
-
-dittoDotPlot(data_ecs_filt, vars = nrf2_targets, group.by  = "label",
-             scale = FALSE, size = 12) + theme(text = element_text(size = 25)) 
-
-
-multi_dittoPlot(data_ecs_filt, vars = markers_bar_int, 
                 group.by = "label", plots = c("jitter", "vlnplot", "boxplot"), 
                 ylab = "Lognormalized counts", ncol = 3,
                 theme = theme_classic() + theme(plot.title = element_text(size = 10)))
